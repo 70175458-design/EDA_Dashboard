7fdefeadf0efdf59d8c16d73b92e1f1463478804
70175458-design <70175458@student.uol.edu.pk>
2026-06-03 22:57:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,9 @@ type=["csv"]
 )
 
 if uploaded_file is not None:
-
-```
 df = pd.read_csv(uploaded_file)
 
+```
 filtered_df = sidebar_filters(df)
 
 st.success("Dataset Loaded Successfully")
@@ -27,7 +26,7 @@ st.write("Rows:", filtered_df.shape[0])
 st.write("Columns:", filtered_df.shape[1])
 
 st.dataframe(filtered_df.head())
-```
+
 
 else:
 st.info("Please upload your CSV file.")
