6b302d32d609cc224dea726f4560a1a7974333f5
70175458-design <70175458@student.uol.edu.pk>
2026-06-03 23:20:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 
@@ -6,17 +5,11 @@ st.set_page_config(page_title="EdStats Dashboard")
 
 st.title("📊 EdStats Dashboard")
 
-uploaded_file = st.file_uploader("Upload CSV File", type=["csv"])
+df = pd.read_csv("EdStatsData_Sample.csv")
 
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
+st.success("Dataset Loaded Successfully!")
 
-    st.success("File uploaded successfully!")
+st.write("Rows:", df.shape[0])
+st.write("Columns:", df.shape[1])
 
-    st.write("Rows:", df.shape[0])
-    st.write("Columns:", df.shape[1])
-
-    st.dataframe(df.head())
-
-else:
-    st.info("Please upload a CSV file.")
+st.dataframe(df.head())
