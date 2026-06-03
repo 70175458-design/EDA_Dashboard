fe6623339ec8439606fefcca12d779c1039c8cbd
70175458-design <70175458@student.uol.edu.pk>
2026-06-03 23:29:25 +0500

    Update charts.py

--- charts.py
+++ charts.py
@@ -5,7 +5,7 @@ def bar_chart(df):
 region_avg = df.groupby("Region")["Value"].mean().dropna()
 
 ```
-fig, ax = plt.subplots(figsize=(10,5))
+fig, ax = plt.subplots(figsize=(10, 5))
 region_avg.sort_values().plot(kind="barh", ax=ax)
 
 ax.set_title("Average Value by Region")
