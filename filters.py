261818a3dedcc4fc85e780afc2aa9fd5f1fefd24
70175458-design <70175458@student.uol.edu.pk>
2026-06-03 23:35:10 +0500

    Update filters.py

--- filters.py
+++ filters.py
@@ -1,16 +1,15 @@
 import streamlit as st
 
 def sidebar_filters(df):
+if "Region" in df.columns:
+regions = st.sidebar.multiselect(
+"Region",
+sorted(df["Region"].dropna().unique()),
+default=sorted(df["Region"].dropna().unique())
+)
+df = df[df["Region"].isin(regions)]
 
 ```
-if "Region" in df.columns:
-    regions = st.sidebar.multiselect(
-        "Region",
-        sorted(df["Region"].dropna().unique()),
-        default=sorted(df["Region"].dropna().unique())
-    )
-    df = df[df["Region"].isin(regions)]
-
 if "Income Group" in df.columns:
     income = st.sidebar.multiselect(
         "Income Group",
@@ -20,4 +19,6 @@ if "Income Group" in df.columns:
     df = df[df["Income Group"].isin(income)]
 
 return df
+```
+
 
