3d3660cb94161548b0e38f673bca15b30d60d22e
70175458-design <70175458@student.uol.edu.pk>
2026-06-03 23:36:29 +0500

    Update filters.py

--- filters.py
+++ filters.py
@@ -1,24 +1,20 @@
 import streamlit as st
 
 def sidebar_filters(df):
-if "Region" in df.columns:
-regions = st.sidebar.multiselect(
-"Region",
-sorted(df["Region"].dropna().unique()),
-default=sorted(df["Region"].dropna().unique())
-)
-df = df[df["Region"].isin(regions)]
+    if "Region" in df.columns:
+        regions = st.sidebar.multiselect(
+            "Region",
+            sorted(df["Region"].dropna().unique()),
+            default=sorted(df["Region"].dropna().unique())
+        )
+        df = df[df["Region"].isin(regions)]
 
+    if "Income Group" in df.columns:
+        income = st.sidebar.multiselect(
+            "Income Group",
+            sorted(df["Income Group"].dropna().unique()),
+            default=sorted(df["Income Group"].dropna().unique())
+        )
+        df = df[df["Income Group"].isin(income)]
 
-if "Income Group" in df.columns:
-    income = st.sidebar.multiselect(
-        "Income Group",
-        sorted(df["Income Group"].dropna().unique()),
-        default=sorted(df["Income Group"].dropna().unique())
-    )
-    df = df[df["Income Group"].isin(income)]
-
-return df
-```
-
-
+    return df
